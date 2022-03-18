f8fa7d032f5441e1e3a9831c646ac724a725b360
rohitdas13595 <rohitdas10595@gmail.com>
2022-03-18 19:56:21 +0530

    Stagged Bsic blocks of the app and Created Header Component.

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import MealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>My Meals</h1>
+        <HeaderCartButton>Cart</HeaderCartButton>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={MealsImage} alt="A table full of healthy food" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
