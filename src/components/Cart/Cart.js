fd9f5e80600eb87a1fecc5602a16d5ddb2022fdd
rohitdas13595 <rohitdas10595@gmail.com>
2022-03-20 21:02:47 +0530

    Added remove Item functionality on the cart Item

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,9 +9,13 @@ const Cart = (props) => {
   const totalAmount = `\u20B9 ${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-  const cartItemAddHandler = (item) => {};
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item,amount:1})
+  };
 
   const cartIems = (
     <ul className={classes["cart-items"]}>
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -8,25 +8,25 @@ const DUMMY_MEALS = [
     id: "m1",
     name: "Sushi",
     description: "Finest fish and veggies",
-    price: 22.99,
+    price: 220.99,
   },
   {
     id: "m2",
     name: "Schnitzel",
     description: "A german specialty!",
-    price: 16.5,
+    price: 160.5,
   },
   {
     id: "m3",
     name: "Barbecue Burger",
     description: "American, raw, meaty",
-    price: 12.99,
+    price: 120.99,
   },
   {
     id: "m4",
     name: "Green Bowl",
     description: "Healthy...and green...",
-    price: 18.99,
+    price: 180.99,
   },
 ];
 
